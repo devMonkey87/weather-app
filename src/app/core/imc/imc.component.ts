5c944d01275d4aad6b9bb4c7f23029f06a7122b4
devMonkey87 <jlandetemunoz@icloud.com>
2022-03-23 20:47:34 +0100

    :recycle: form init

--- src/app/core/imc/imc.component.ts
+++ src/app/core/imc/imc.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-imc',
   templateUrl: './imc.component.html',
-  styleUrls: ['./imc.component.scss']
+  styleUrls: ['./imc.component.scss'],
 })
 export class ImcComponent implements OnInit {
-
-  constructor() { }
+  resultado: string = ''; //FIXME mejorar error messages
+  form: any;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.form = this.fb.group({
+      height: ['', [Validators.required]],
+      weight: ['', [Validators.required]],
+    });
+  }
+
+  submit() {
+    if (this.form.valid) this.resultado = 'Todos los datos son válidos';
+    else this.resultado = 'Hay datos inválidos en el formulario';
+  }
 }
