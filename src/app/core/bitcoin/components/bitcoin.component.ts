d26c237fa60b958a1f270d27ed304db84f389239
devMonkey87 <jlandetemunoz@icloud.com>
2022-05-31 11:09:48 +0200

    add new columns for Bitcoins component

--- src/app/core/bitcoin/components/bitcoin.component.ts
+++ src/app/core/bitcoin/components/bitcoin.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { TableTypes } from 'src/app/shared/shared/table/constants/enums/TableTypes.enum';
 import { TableColumn } from 'src/app/shared/shared/table/constants/interfaces';
-import { columnConfig, elementConfig } from '../constants';
+import { columnConfig } from '../constants';
+import { IBitcoinAction } from '../interfaces/IBitcoinAction';
+import { BitcoinService } from '../services/bitcoin.service';
 
 @Component({
   selector: 'app-bitcoin',
@@ -9,18 +12,34 @@ import { columnConfig, elementConfig } from '../constants';
   styleUrls: ['./bitcoin.component.scss'],
 })
 export class BitcoinComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly bitcoinService: BitcoinService) {}
 
   public tableStyleClass: TableTypes | undefined;
   public tableColumns: TableColumn[] = [];
   public tableElements: any[] = [];
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.setupTable();
   }
+
   setupTable() {
-    this.tableStyleClass = TableTypes.Dark;
     this.tableColumns = columnConfig();
-    this.tableElements = elementConfig();
+    this.bitcoinService.getData().then(
+      (bitcoin) =>
+        (this.tableElements = bitcoin.map((bitcoinAction) => {
+          const openTimeDate = new Date(bitcoinAction.openTime);
+          const closeTimeDate = new Date(bitcoinAction.closeTime);
+          const bAction: IBitcoinAction = {
+            openTime: openTimeDate.toLocaleDateString('en-GB'),
+            closeTime: closeTimeDate.toLocaleDateString('en-GB'),
+            openPrice: bitcoinAction.openPrice,
+            lastPrice: bitcoinAction.lastPrice,
+            symbol: bitcoinAction.symbol,
+            priceChangePercent: bitcoinAction.priceChangePercent,
+          }; //TODO: make it more efficient with restOfProperties assignation, only date 
+          // properties need to be adapted
+          return bAction;
+        }))
+    );
   }
 }
--- src/app/shared/shared/table/constants/interfaces/index.ts
+++ src/app/shared/shared/table/constants/interfaces/index.ts
@@ -1,5 +1,6 @@
 export interface TableColumn {
-  title: string;
+  id: string;
+  label: string;
   isDisabled?: boolean;
 }
 
