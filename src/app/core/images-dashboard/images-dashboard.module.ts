0f9911d127137c2634bfa5b19f8e7695db8b1bb8
devMonkey87 <jlandetemunoz@icloud.com>
2022-06-22 10:21:15 +0200

    :+1: add service for images plus table clean-up

--- src/app/core/images-dashboard/images-dashboard.module.ts
+++ src/app/core/images-dashboard/images-dashboard.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ImagesDashboardComponent } from './components/images-dashboard.component';
 import { ImagesDashboardRoutingModule } from './components/images-dashboard-routing.module';
+import { PictureService } from './services/picture.service';
 
 @NgModule({
   declarations: [ImagesDashboardComponent],
   imports: [CommonModule, SharedModule, ImagesDashboardRoutingModule],
   exports: [ImagesDashboardComponent],
+  providers: [PictureService],
 })
 export class ImagesDashboardModule {}
