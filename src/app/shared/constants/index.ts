8f2d78a99ed18268155def4b519d8ee03e75b615
devMonkey87 <jlandetemunoz@icloud.com>
2022-07-31 15:33:12 +0200

    ♻️ rapid api constants refactor

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -1,1 +1,9 @@
-export const LOCALHOST_BASE_URL = "http://localhost:8080/"
+export const LOCALHOST_BASE_URL = 'http://localhost:8080/';
+
+export const RAPID_API_HOST = 'X-RapidAPI-Host';
+export const RAPID_API_KEY = 'X-RapidAPI-Key';
+export const RAPID_API_KEY_PASSWORD ='94ee14ffabmshfbfd83af1fe237ep166202jsn5bd8d48365f2';
+// const RAPID_API_BMI = 'X-RapidAPI-Host';
+// const RAPID_API_BMI = 'X-RapidAPI-Host';
+// const RAPID_API_BMI = 'X-RapidAPI-Host';
+// const RAPID_API_BMI = 'X-RapidAPI-Host';
