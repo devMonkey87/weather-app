bc29ea7acf588fa371a35561161e5e714055ec61
devMonkey87 <jlandetemunoz@icloud.com>
2022-08-08 20:54:03 +0200

    :100: add new component for holding dynamic form components

---
+++ src/app/shared/dynamic-form-modal/dynamic-form-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-form-modal',
+  templateUrl: './dynamic-form-modal.component.html',
+  styleUrls: ['./dynamic-form-modal.component.scss']
+})
+export class DynamicFormModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
