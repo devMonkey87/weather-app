46aefe16f227e3f3b94ca236a40183498d40d87c
devMonkey87 <jlandetemunoz@icloud.com>
2023-01-19 14:59:02 +0100

    refactor

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -1,14 +1,13 @@
+import { RAPID_API_KEY_PASSWORD } from '../../../config';
+
 export const LOCALHOST_BASE_URL = 'http://localhost:8080/';
 
 export const RAPID_API_HOST = 'X-RapidAPI-Host';
 export const RAPID_API_KEY = 'X-RapidAPI-Key';
-export const RAPID_API_KEY_PASSWORD = '94ee14ffabmshfbfd83af1fe237ep166202jsn5bd8d48365f2';
 
-export const RAPID_API_BMI_HOST_VALUE = 'body-mass-index-bmi-calculator.p.rapidapi.com';
+export const RAPID_API_KEY_PW = RAPID_API_KEY_PASSWORD;
+export const RAPID_API_BMI_HOST_VALUE =
+  'body-mass-index-bmi-calculator.p.rapidapi.com';
 export const RAPID_API_BITCOIN_HOST_VALUE = 'binance43.p.rapidapi.com';
-export const RAPID_API_WEATHER_HOST_VALUE = 'weatherbit-v1-mashape.p.rapidapi.com'
-
-// const RAPID_API_BMI = 'X-RapidAPI-Host';
-// const RAPID_API_BMI = 'X-RapidAPI-Host';
-// const RAPID_API_BMI = 'X-RapidAPI-Host';
-// const RAPID_API_BMI = 'X-RapidAPI-Host';
+export const RAPID_API_WEATHER_HOST_VALUE =
+  'weatherbit-v1-mashape.p.rapidapi.com';
